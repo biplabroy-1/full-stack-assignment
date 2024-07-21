d9713e4a8a1a913305ffc30404236e32e5cebd8c
Biplab Roy <biplabr119201@gmail.com>
2024-07-21 19:47:52 +0530

    added all fixed

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ const port = 3001
 const USERS = [];
 
 const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+    input: "[1,2,3,4,5]",
+    output: "5"
+  }]
 }];
 
 
-const SUBMISSION = [
+const SUBMISSION = []
 
-]
-
-app.post('/signup', function(req, res) {
+app.post('/signup', function (req, res) {
   // Add logic to decode body
   // body should have email and password
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+    prem: "user"
+  }
+  USERS.push(user);
 
 
   //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
@@ -30,37 +34,42 @@ app.post('/signup', function(req, res) {
   res.send('Hello World!')
 })
 
-app.post('/login', function(req, res) {
+app.post('/login', function (req, res) {
   // Add logic to decode body
   // body should have email and password
+  const email = req.body.email;
+  const password = req.body.password;
+  // Check if the user exists in the USERS array above
+  const user = USERS.find(user => user.email === email && user.password === password);
+  if (user) {
+    // If the user exists, return back 200 status code to the client
+    // Also send back a token (any random string will do for now)
+    let token = Math.floor(Math.random() * 100())
+    res.status(200).send("User Found")
+  } else {
+    // If the user does not exist, return back 401 status code to the client
+    res.send('User Not Found')
+  }
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure that the password is the same
-
-
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
-
-
-  res.send('Hello World from route 2!')
 })
 
-app.get('/questions', function(req, res) {
+app.get('/questions', function (req, res) {
 
   //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
+  res.send(QUESTIONS)
 })
 
-app.get("/submissions", function(req, res) {
-   // return the users submissions for this problem
-  res.send("Hello World from route 4!")
+app.get("/submissions", function (req, res) {
+  // return the users submissions for this problem
+  res.send(SUBMISSION)
 });
 
 
-app.post("/submissions", function(req, res) {
-   // let the user submit a problem, randomly accept or reject the solution
-   // Store the submission in the SUBMISSION array above
+app.post("/submissions", function (req, res) {
+
+
+  // let the user submit a problem, randomly accept or reject the solution
+  // Store the submission in the SUBMISSION array above
   res.send("Hello World from route 4!")
 });
 
@@ -68,6 +77,29 @@ app.post("/submissions", function(req, res) {
 // Create a route that lets an admin add a new problem
 // ensure that only admins can do that.
 
-app.listen(port, function() {
+app.post("/admin", function (req, res) {
+  // Add logic to decode body
+  // body should have email and password
+  const email = req.body.email;
+  const password = req.body.password;
+  // Check if the user exists in the USERS array above
+  const user = USERS.find(user => user.email === email && user.password === password && user.prem === "admin");
+  if (user) {
+
+    const quest = {
+      title: req.body.title,
+      question: req.body.question,
+    }
+    QUESTIONS.push(quest);
+
+  } else {
+    // If the user does not exist, return back 401 status code to the client
+    res.send('User Not Found')
+  }
+
+
+})
+
+app.listen(port, function () {
   console.log(`Example app listening on port ${port}`)
 })
